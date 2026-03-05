3c497a6bc82e13258a0fb574fdc324008179e9fa
Inchara nk <incharank1@gmail.com>
2026-03-05 22:09:20 +0530

    last commit

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 async function getWeather(){
 
-let city=document.getElementById("cityInput").value;
+let city = document.getElementById("cityInput").value;
 
-let result=document.getElementById("weatherResult");
+let result = document.getElementById("weatherResult");
 
-if(city===""){
-result.innerHTML="Please enter a city name";
+if(city === ""){
+result.innerHTML = "Please enter a city name";
 return;
 }
 
-result.innerHTML="Loading weather... ⏳";
+result.innerHTML = "Loading weather... ⏳";
 
-let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=154512e8f73de2b7ae73c42cfbc91143`;
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=154512e8f73de2b7ae73c42cfbc91143`;
 
 try{
 
-let response=await fetch(url);
+let response = await fetch(url);
 
-let data=await response.json();
+let data = await response.json();
 
-if(data.cod==404){
-result.innerHTML="City not found ❌";
+if(data.cod == 404){
+result.innerHTML = "City not found ❌";
 return;
 }
 
-let temp=data.main.temp;
+let temp = data.main.temp;
 
-let weather=data.weather[0].main;
+let weather = data.weather[0].main;
 
-let icon=data.weather[0].icon;
+let icon = data.weather[0].icon;
 
-let iconUrl=`https://openweathermap.org/img/wn/${icon}@2x.png`;
+let iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
 
-result.innerHTML=`
+result.innerHTML = `
 <h2>${city}</h2>
 <img src="${iconUrl}">
 <p>Temperature: ${temp}°C</p>
@@ -40,8 +40,9 @@ result.innerHTML=`
 `;
 
 }
+
 catch(error){
-result.innerHTML="Unable to fetch weather ⚠️";
+result.innerHTML = "Unable to fetch weather ⚠️";
 }
 
 }
