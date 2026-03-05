461c81a6bb63fde45342695164cc57a4f46a2562
Inchara nk <incharank1@gmail.com>
2026-03-05 21:55:14 +0530

    updated

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ async function getWeather(){
 
 let city=document.getElementById("cityInput").value;
 
-let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=YOUR_API_KEY&units=metric`;
+let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=154512e8f73de2b7ae73c42cfbc91143`;
+
+try{
 
 let response=await fetch(url);
 
@@ -17,7 +19,21 @@ let temp=data.main.temp;
 
 let weather=data.weather[0].main;
 
+let icon=data.weather[0].icon;
+
+let iconUrl=`https://openweathermap.org/img/wn/${icon}@2x.png`;
+
 document.getElementById("weatherResult").innerHTML=
-`Temperature: ${temp}°C <br> Weather: ${weather}`;
+`
+<h2>${city}</h2>
+<img src="${iconUrl}">
+<p>Temperature: ${temp}°C</p>
+<p>Weather: ${weather}</p>
+`;
+
+}
+catch(error){
+document.getElementById("weatherResult").innerHTML="Error fetching weather";
+}
 
 }
