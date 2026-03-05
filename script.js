e1005eacd95ff71513a70abe511a83ba85225c6e
Inchara nk <incharank1@gmail.com>
2026-03-05 22:00:56 +0530

    Done

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ async function getWeather(){
 
 let city=document.getElementById("cityInput").value;
 
+let result=document.getElementById("weatherResult");
+
+if(city===""){
+result.innerHTML="Please enter a city name";
+return;
+}
+
+result.innerHTML="Loading weather... ⏳";
+
 let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=154512e8f73de2b7ae73c42cfbc91143`;
 
 try{
@@ -10,8 +19,8 @@ let response=await fetch(url);
 
 let data=await response.json();
 
-if(data.cod=="404"){
-document.getElementById("weatherResult").innerHTML="City not found";
+if(data.cod==404){
+result.innerHTML="City not found ❌";
 return;
 }
 
@@ -23,8 +32,7 @@ let icon=data.weather[0].icon;
 
 let iconUrl=`https://openweathermap.org/img/wn/${icon}@2x.png`;
 
-document.getElementById("weatherResult").innerHTML=
-`
+result.innerHTML=`
 <h2>${city}</h2>
 <img src="${iconUrl}">
 <p>Temperature: ${temp}°C</p>
@@ -33,7 +41,7 @@ document.getElementById("weatherResult").innerHTML=
 
 }
 catch(error){
-document.getElementById("weatherResult").innerHTML="Error fetching weather";
+result.innerHTML="Unable to fetch weather ⚠️";
 }
 
 }
